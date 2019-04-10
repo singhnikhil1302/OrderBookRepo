13569df469945c6208c5294427e78d144870db5f
singhnikhil1302 <singh.nikhil002@gmail.com>
2019-04-10 23:17:27 +0530

    Removed unused Imports

--- src/main/java/com/orderbook/springbootrestapiapp/vo/OrderDetails.java
+++ src/main/java/com/orderbook/springbootrestapiapp/vo/OrderDetails.java
@@ -15,10 +15,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.orderbook.springbootrestapiapp.common.JsonDateDeserializer;
 import com.orderbook.springbootrestapiapp.common.JsonDateSerializer;
 import com.orderbook.springbootrestapiapp.common.OrderStatus;
 import com.orderbook.springbootrestapiapp.common.OrderType;
